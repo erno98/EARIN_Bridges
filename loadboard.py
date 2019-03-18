9f5e9ed3412c8a1725d5763342069ce97d9455f1
Ernest Pokropek <34246636+erno98@users.noreply.github.com>
2019-03-18 16:25:54 +0100

    Update loadboard.py

--- loadboard.py
+++ loadboard.py
@@ -1,2 +1,13 @@
-"""tutaj ładowanie z pliku
-"""
+
+def load_map(file_name):
+
+    with open(file_name, 'r') as file:
+        return [list(map(int, row.strip('\n').split(' '))) for row in file]
+    # for each row:
+    #   - remove the '\n' symbol
+    #   - split each element separated by ' '
+    #   - map inside elements to integers
+
+
+puzzle_map = load_map("bridges7x7_1.txt")
+print(puzzle_map)

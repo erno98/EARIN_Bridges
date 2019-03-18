923af63f629c6a45e3c135a4a630af29bd53a0cd
Ernest Pokropek <34246636+erno98@users.noreply.github.com>
2019-03-18 16:25:23 +0100

    Add files via upload

---
+++ load_map.py
@@ -0,0 +1,13 @@
+
+def load_map(file_name):
+
+    with open(file_name, 'r') as file:
+        return [list(map(int, row.strip('\n').split(' '))) for row in file]
+    # for each row:
+    #   - remove the '\n' symbol
+    #   - split each element separated by ' '
+    #   - map inside elements to integers
+
+
+puzzle_map = load_map("bridges7x7_1.txt")
+print(puzzle_map)
